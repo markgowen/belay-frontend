64837bb8713abdf76aec0e6e365e324c694a6ed8
Mark Gowen <mark.gowen78@gmail.com>
2022-12-07 08:16:48 -0800

    Created routes

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import '../styling/App.css';
 import NavBar from '../components/NavBar';
 import Discover from './Discover';
+import Climb from "./Climb";
+import Home from "./Home";
+import Share from "./Share";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <header>
+      <Router>
         <div>
           <NavBar />
-          <Discover />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/discover" component={Discover} />
+            <Route path="/climb" exact component={Climb} />
+            <Route path="/share" exact component={Share} />
+          </Switch>
         </div>
-      </header>
-    </div>
+      </Router>
   );
 }
 
