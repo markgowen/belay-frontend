16095f7c915a579b490fdb8c57fe9c2d06686d55
Mark <mark.gowen78@gmail.com>
2022-12-08 10:00:37 -0800

    Fixed controlled form issue

--- src/components/ClimbForm.js
+++ src/components/ClimbForm.js
@@ -33,6 +33,7 @@ function ClimbForm() {
       ];
 
     const handleChange = (e) => {
+      console.log(e.target.value);
         setFormValues({...formValues, [e.target.name]: e.target.value})
     }
 
@@ -54,68 +55,67 @@ function ClimbForm() {
             autoComplete="off"
             onSubmit={handleSubmit}
         >
-        <TextField
-          label="Route Name"
-          id="standard-size-normal"
-          value={formValues.name}
-          onChange={handleChange}
-          variant="standard"
-        />
-        <TextField
-          label="Location"
-          id="standard-size-normal"
-          value={formValues.location}
-          onChange={handleChange}
-          noValidate
-          defaultValue=""
-          variant="standard"
-        />
-        <TextField
-          id="standard-multiline-flexible"
-          label="Description"
-          multiline
-          maxRows={4}
-          value={formValues.description}
-          onChange={handleChange}
-          variant="standard"
-        />
-         <TextField
-          id="standard-number"
-          label="Elevation"
-          value={formValues.elevation}
-          onChange={handleChange}
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          label="Grade"
-          id="standard-size-normal"
-          value={formValues.difficulty}
-          onChange={handleChange}
-          noValidate
-          defaultValue="5.10b"
-          variant="standard"
-        />
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Select"
-          sx={{ color: "#1e1e1e" }}
-          value={type}
-          onChange={handleChange}
-          helperText=""
-          variant="standard"
-        >
-          {type.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <Button sx={{ backgroundColor: "#FF8A00" }} variant="contained">Sumbit</Button>
+          <TextField
+            helperText="Route Name"
+            name="name"
+            defaultValue={formValues.name}
+            id="standard-size-normal"
+            onChange={handleChange}
+            variant="standard"
+          />
+          <TextField
+            helperText="Location"
+            id="standard-size-normal"
+            value={formValues.location}
+            onChange={handleChange}
+            noValidate
+            variant="standard"
+          />
+          <TextField
+            id="standard-multiline-flexible"
+            helperText="Description"
+            multiline
+            maxRows={4}
+            value={formValues.description}
+            onChange={handleChange}
+            variant="standard"
+          />
+          <TextField
+            id="standard-number"
+            helperText="Elevation"
+            value={formValues.elevation}
+            onChange={handleChange}
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="standard"
+          />
+          <TextField
+            helperText="Grade"
+            id="standard-size-normal"
+            value={formValues.difficulty}
+            onChange={handleChange}
+            noValidate
+            variant="standard"
+          />
+          <TextField
+            id="standard-select-currency"
+            select
+            label="Select"
+            sx={{ color: "#1e1e1e" }}
+            value={formValues.type}
+            onChange={handleChange}
+            helperText=""
+            variant="standard"
+          >
+            {type.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <Button sx={{ backgroundColor: "#FF8A00" }} variant="contained">Sumbit</Button>
         </Box>
     );
 };
