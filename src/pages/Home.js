befa52914c538b85c4d56ae9644552e585c198e0
Mark <mark.gowen78@gmail.com>
2022-12-08 15:34:14 -0800

    bottom section of home page styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import solo from "../assets/images/solo.jpg";
-import logo from "../assets/logos/Logo.png"
+import logo from "../assets/logos/Logo.png";
+import Button from '@mui/material/Button';
 
 function Home() {
     return (
@@ -25,7 +26,17 @@ function Home() {
                 <div className='middle'>
                     <img src={solo} alt='climber' />
                 </div>
-                    <img src={logo} alt='belay logo' />
+                <div className='bottom'>
+                    <div className='bottom-logo'>
+                        <img src={logo} alt='belay logo' />
+                    </div>
+                    <div className='bottom-section'>
+                        <h3>Climb More</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <Button sx={{ backgroundColor: "#FF8A00" }} variant="contained" type="submit" >Discover</Button>
+                    </div>
+                </div>
             </div>
         </>
     )
