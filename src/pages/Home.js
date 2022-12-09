c3788661591ad2dfef8ded001a6c3ce7915f56f8
Mark Gowen <mark.gowen78@gmail.com>
2022-12-09 10:27:23 -0800

    Styled home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import solo from "../assets/images/solo.jpg";
 import logo from "../assets/logos/Logo.png";
 import Button from '@mui/material/Button';
+import blackSquare from "../assets/logos/blackSquare.png";
+import orangSquare from "../assets/logos/OrangeSquare.png";
+import greenSquare from "../assets/logos/greenSquare.png";
 
 function Home() {
     return (
@@ -9,18 +12,28 @@ function Home() {
             <div className='hero'>
                 <h1>Plan Your Next Climbing Adventure</h1>
             </div>
-            <div className='secondary-hero'>
-                <h2>Discover</h2>
-                <h2>Plan</h2>
-                <h2>Share</h2>
-            </div>
-            <div className=''>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
+            <div className='columns'>
+                <div className='hero-column'> 
+                    <img src={blackSquare} alt={blackSquare} />
+                    <h2>Discover</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <Button sx={{ backgroundColor: "#000" }} variant="contained" type="submit" >Discover</Button>
+                </div>
+                <div className='hero-column'> 
+                    <img src={orangSquare} alt={blackSquare} />
+                    <h2>Plan</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <Button sx={{ backgroundColor: "#FF8A00" }} variant="contained" type="submit" >Discover</Button>
+                </div>
+                <div className='hero-column'> 
+                    <img src={greenSquare} alt={blackSquare} />
+                    <h2>Share</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <Button sx={{ backgroundColor: "#2F642A" }} variant="contained" type="submit" >Discover</Button>
+                </div>
             </div>
             <div className='middle-section'>
                 <div className='middle'>
